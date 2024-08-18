b4d6f4ca8f72dbe078a778137f52b5f1acef137f
RaddishAI <petter.arbeid@gmail.com>
2024-08-18 16:04:31 +0200

    added api fetch and some pictures

--- scripts.js
+++ scripts.js
@@ -1,1 +1,15 @@
 const apiUrl = "https://blog.raddishai.no/wp-json/wp/v2/recipe";
+
+fetch(apiUrl)
+    .then(Response => {
+        if (!Response.ok) {
+            throw new Error ("API call respone was not ok");
+        }
+        return Response.json();
+    })
+    .then(userData => {
+        console.log("User data: ", userData);
+    })
+    .catch(error => {
+        console.error("Error:", error);
+    });
