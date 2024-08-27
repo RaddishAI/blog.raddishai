f283704c8c8982a0385aae3d8f7ebea35214e5dc
RaddishAI <petter.arbeid@gmail.com>
2024-08-27 20:47:55 +0200

    added extention on per page on the API return

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-const apiUrl = "https://blog.raddishai.no/wp-json/wp/v2/recipe";
+const apiUrl = "https://blog.raddishai.no/wp-json/wp/v2/recipe?per_page=15";
 
 fetch(apiUrl)
     .then(Response => {
